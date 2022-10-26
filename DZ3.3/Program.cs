3a68266150cea2c8b52e88689bea6b8d9946f0b5
Ramwh <vasilkovd_67@mail.ru>
2022-10-26 22:13:09 +0400

    Добавлено решение третьей задачи

---
+++ DZ3.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {   
+            int[] cube(int n)
+            {
+                  
+                int[] cubes = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    cubes[i] = (i+1)*(i+1)*(i+1);
+                }
+                return cubes;
+                
+            }
+            Console.Write("Введите положительно целое число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num <= 0)
+            {
+                Console.WriteLine("Введённое число должно быть больше 0 ");
+            }
+            else 
+            {
+                int[] array = cube(num);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+            }
+          
+        }
+    }
+}
